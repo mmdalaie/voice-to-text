3769026fb7324f6eb8e93111a33bdbd28067b591
mmdalaie <mmdalaie93@gmail.com>
2024-05-29 01:01:29 +0330

    upload file

--- vtt.py
+++ vtt.py
@@ -7,13 +7,12 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Listening...")
     # read the audio data from the default microphone
-    audio_data = r.record(source, duration=5)
+    audio_data = r.record(source, duration=10)
     print("Recognizing...")
     # convert speech to text
-    text = r.recognize_google(audio_data)
+    text = r.recognize_google(audio_data, language='fa-IR')
     print(text)
-
 # save the text to a .txt file
-with open('output.txt', 'w') as f:
+with open('output1.txt', 'w',encoding='utf-8') as f:
     f.write(text)
 
